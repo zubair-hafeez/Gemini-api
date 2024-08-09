6c7a342fe2e973e221f13bf07c7a04b7891c3ad4
Zubair Hafeez <jamzubair88@gmail.com>
2024-08-09 15:12:40 +0500

    initialize

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+import streamlit as st
+
+GOOGLE_API_KEY = "AIzaSyCRcGr5HK8ltQzczWEg9Fc0aF0hTM0C8IM"
+genai.configure(api_key= GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+def getResponseFromModel(user_input):
+    response = model.generate_content(user_input)
+    return response.text
+
+st.title("Simple ChatBot")
+st.write ("It uses Google API key")
+
+user_input = input ('Enter your Prompt = ')
+output = getResponseFromModel(user_input)
+
+print(output)
